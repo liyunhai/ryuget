71f0318fc57f60e266da65b22c3b6d652e766518
Derrek Ramos <Derrekito@gmail.com>
2013-08-15 18:41:12 -0500

    small change

--- ryuget.py
+++ ryuget.py
@@ -1,13 +1,13 @@
 import os
 
 os.chdir('/your/dir/path/of/choice/')
+
 file = "list"
-
 list = [i.strip() for i in open(file).readlines()]
-
 
 for i in range(len(list)):
     os.system("wget --load-cookies cookies.txt "+list[i])
+
 
 if __name__ == '__main__':
   main()
